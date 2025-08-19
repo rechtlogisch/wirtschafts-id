728d0544c88fd7bac0986fcae06bb811471f33e0
Krzysztof Tomasz Zembrowski <zembrowski@users.noreply.github.com>
2025-08-19 15:47:47 +0200

    chore: BZSt informed, that issuing the Unterscheidungsmerkmal will start in the 4th Quarter of 2027

--- src/Exceptions/UnterscheidungsmerkmalShouldBe00001BeforeYear2026.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Rechtlogisch\WirtschaftsId\Exceptions;
-
-class UnterscheidungsmerkmalShouldBe00001BeforeYear2026 extends WirtschaftsIdConstraintException {}
